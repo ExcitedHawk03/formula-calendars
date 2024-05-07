5137806b58af556a70fd6ecf7eb83cf7f0dd3266
ExcitedHawk03 <joeytoet14@gmail.com>
2024-05-07 20:22:37 +0200

    Added footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,23 @@
+<script setup></script>
+
+<template>
+  <footer>
+    <div>&copy; {{ new Date().getFullYear() }} Copyright: Motorsport Kalender</div>
+  </footer>
+</template>
+
+<style scoped>
+footer {
+  position: fixed;
+  bottom: 0;
+  background-color: blue;
+  width: 100%;
+}
+
+footer div {
+  color: white;
+  padding: 15px;
+  text-align: center;
+  font-size: larger;
+}
+</style>
--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -1,10 +1,12 @@
 <script setup>
 import Header from '@/components/Header.vue'
+import Footer from '@/components/Footer.vue'
 </script>
 
 <template>
   <Header title="Homepagina" />
   <main></main>
+  <Footer />
 </template>
 
 <style scoped></style>
