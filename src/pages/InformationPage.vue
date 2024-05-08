257b051c9e3389416cc1b95c8d22a5bab06e2eef
ExcitedHawk03 <joeytoet14@gmail.com>
2024-05-08 23:12:11 +0200

    Added Information Page

---
+++ src/pages/InformationPage.vue
@@ -0,0 +1,18 @@
+<script>
+import Footer from '@/components/Footer.vue'
+import Header from '@/components/Header.vue'
+
+export default {
+  name: 'Information Page',
+  components: {
+    Header,
+    Footer
+  }
+}
+</script>
+
+<template>
+  <Header title="Information Page" />
+  <main></main>
+  <Footer />
+</template>
