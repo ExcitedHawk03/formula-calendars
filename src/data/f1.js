7cff53a171eaadf313722ebcde19672d09f9d7dc
Joey Toet <76113253+ExcitedHawk03@users.noreply.github.com>
2024-05-10 10:34:14 +0200

    Changed some things (#2)
    * Changed some things
    
    * Changed data

--- src/data/f1.js
+++ src/data/f1.js
@@ -20,148 +20,148 @@ import brazilImg from '../assets/images/flags/brazil.jpeg'
 import qatarImg from '../assets/images/flags/qatar.jpeg'
 import abuDhabiImg from '../assets/images/flags/abuDhabi.jpeg'
 
-export const circuits = [
+export const grandPrixs = [
   {
     name: 'Grand Prix van Bahrein',
-    date: '02-03-2024',
+    date: '29-02-2023 t/m 02-03-2024',
     image: bahrainImg,
     link: 'Bahrain'
   },
   {
     name: 'Grand Prix van Saudi Arabië',
-    date: '09-03-2024',
+    date: '07-03-2024 t/m 09-03-2024',
     image: saudiArabiaImg,
     link: 'Saudi_Arabia'
   },
   {
     name: 'Grand Prix van Australië',
-    date: '24-04-2024',
+    date: '22-03-2024 t/m 24-03-2024',
     image: australiaImg,
     link: 'Australia'
   },
   {
     name: 'Grand Prix van Japan',
-    date: '07-04-2024',
+    date: '05-04-2024 t/m 07-04-2024',
     image: japanImg,
     link: 'Japan'
   },
   {
     name: 'Grand Prix van China',
-    date: '21-04-2024',
+    date: '19-04-2024 t/m 21-04-2024',
     image: chinaImg,
     link: 'China'
   },
   {
     name: 'Grand Prix van Miami',
-    date: '05-05-2024',
+    date: '03-05-2024 t/m 05-05-2024',
     image: unitedStatesImg,
     link: 'Miami'
   },
   {
     name: 'Grand Prix van Emilia-Romagna',
-    date: '19-05-2024',
+    date: '17-05-2024 t/m 19-05-2024',
     image: italyImg,
     link: 'EmiliaRomagna'
   },
   {
     name: 'Grand Prix van Monaco',
-    date: '26-05-2024',
+    date: '24-05-2024 t/m 26-05-2024',
     image: monacoImg,
     link: 'Monaco'
   },
   {
     name: 'Grand Prix van Canada',
-    date: '09-06-2024',
+    date: '07-06-2024 t/m 09-06-2024',
     image: canadaImg,
     link: 'Canada'
   },
   {
     name: 'Grand Prix van Spanje',
-    date: '23-06-2024',
+    date: '21-06-2024 t/m 23-06-2024',
     image: spainImg,
     link: 'Spain'
   },
   {
     name: 'Grand Prix van Oostenrijk',
-    date: '30-06-2024',
+    date: '28-06-2024 t/m 30-06-2024',
     image: austriaImg,
     link: 'Austria'
   },
   {
     name: 'Grand Prix van Groot-Brittannië',
-    date: '07-07-2024',
+    date: '05-07-2024 t/m 07-07-2024',
     image: greatBritainImg,
     link: 'Great_Britain'
   },
   {
     name: 'Grand Prix van Hungary',
-    date: '21-07-2024',
+    date: '19-07-2024 t/m 21-07-2024',
     image: hungaryImg,
     link: 'Hungary'
   },
   {
     name: 'Grand Prix van België',
-    date: '28-07-2024',
+    date: '26-07-2024 t/m 28-07-2024',
     image: belgiumImg,
     link: 'Belgium'
   },
   {
     name: 'Grand Prix van Nederland',
-    date: '25-08-2024',
+    date: '23-08-2024 t/m 25-08-2024',
     image: netherlandsImg,
     link: 'Netherlands'
   },
   {
     name: 'Grand Prix van Italië',
-    date: '01-09-2024',
+    date: '30-08-2024 t/m 01-09-2024',
     image: italyImg,
     link: 'Italy'
   },
   {
     name: 'Grand Prix van Azerbeidzjan',
-    date: '15-09-2024',
+    date: '13-09-2024 t/m 15-09-2024',
     image: azerbaijanImg,
     link: 'Azerbaijan'
   },
   {
     name: 'Grand Prix van Singapore',
-    date: '22-09-2024',
+    date: '20-09-2024 t/m 22-09-2024',
     image: singaporeImg,
     link: 'Singapore'
   },
   {
     name: 'Grand Prix van de VS',
-    date: '20-10-2024',
+    date: '18-10-2024 t/m 20-10-2024',
     image: unitedStatesImg,
     link: 'United_States'
   },
   {
     name: 'Grand Prix van Mexico',
-    date: '27-10-2024',
+    date: '25-10-2024 t/m 27-10-2024',
     image: mexicoImg,
     link: 'Mexico'
   },
   {
     name: 'Grand Prix van Brazilië',
-    date: '03-11-2024',
+    date: '01-11-2024 t/m 03-11-2024',
     image: brazilImg,
     link: 'Brazil'
   },
   {
     name: 'Grand Prix van Las Vegas',
-    date: '24-11-2024',
+    date: '21-11-2024 t/m 23-11-2024',
     image: unitedStatesImg,
     link: 'Las_Vegas'
   },
   {
     name: 'Grand Prix van Qatar',
-    date: '01-12-2024',
+    date: '29-11-2024 t/m 01-12-2024',
     image: qatarImg,
     link: 'Qatar'
   },
   {
     name: 'Grand Prix van Abu Dhabi',
-    date: '08-12-2024',
+    date: '06-12-2024 t/m 08-12-2024',
     image: abuDhabiImg,
     link: 'United_Arab_Emirates'
   }
--- src/data/f2.js
+++ src/data/f2.js
@@ -1,114 +1,100 @@
-import bahrainImg from '../assets/images/flags/bahrain.jpeg';
-import saudiArabiaImg from '../assets/images/flags/saudiArabia.jpeg';
-import australiaImg from '../assets/images/flags/australia.jpeg';
-import italyImg from '../assets/images/flags/italy.jpeg';
-import monacoImg from '../assets/images/flags/monaco.jpeg';
-import spainImg from '../assets/images/flags/spain.jpeg';
-import austriaImg from '../assets/images/flags/austria.jpeg';
-import greatBritainImg from '../assets/images/flags/greatBritain.jpeg';
-import hungaryImg from '../assets/images/flags/hungary.jpeg';
-import belgiumImg from '../assets/images/flags/belgium.jpeg';
-import azerbaijanImg from '../assets/images/flags/azerbeidzjan.jpeg';
-import qatarImg from '../assets/images/flags/qatar.jpeg';
-import abuDhabiImg from '../assets/images/flags/abuDhabi.jpeg';
+import bahrainImg from '../assets/images/flags/bahrain.jpeg'
+import saudiArabiaImg from '../assets/images/flags/saudiArabia.jpeg'
+import australiaImg from '../assets/images/flags/australia.jpeg'
+import italyImg from '../assets/images/flags/italy.jpeg'
+import monacoImg from '../assets/images/flags/monaco.jpeg'
+import spainImg from '../assets/images/flags/spain.jpeg'
+import austriaImg from '../assets/images/flags/austria.jpeg'
+import greatBritainImg from '../assets/images/flags/greatBritain.jpeg'
+import hungaryImg from '../assets/images/flags/hungary.jpeg'
+import belgiumImg from '../assets/images/flags/belgium.jpeg'
+import azerbaijanImg from '../assets/images/flags/azerbeidzjan.jpeg'
+import qatarImg from '../assets/images/flags/qatar.jpeg'
+import abuDhabiImg from '../assets/images/flags/abuDhabi.jpeg'
 
-export const circuits = [
+export const grandPrixs = [
   {
     round: 1,
     name: 'Grand Prix van Bahrein',
-    date: '02-03-2024',
-    image: bahrainImg,
-    link: 1064
+    date: '29-02-2024 t/m 02-03-2024',
+    image: bahrainImg
   },
   {
     round: 2,
     name: 'Grand Prix van Saudi Arabië',
-    date: '09-03-2024',
-    image: saudiArabiaImg,
-    link: 1065
+    date: '07-03-2024 t/m 09-03-2024',
+    image: saudiArabiaImg
   },
   {
     round: 3,
     name: 'Grand Prix van Australië',
-    date: '24-03-2024',
-    image: australiaImg,
-    link: 1066
+    date: '22-03-2024 t/m 24-03-2024',
+    image: australiaImg
   },
   {
     round: 4,
     name: 'Grand Prix van Emilia Romagna',
-    date: '19-05-2024',
-    image: italyImg,
-    link: 1067
+    date: '17-05-2024 t/m 19-05-2024',
+    image: italyImg
   },
   {
     round: 5,
     name: 'Grand Prix van Monaco',
-    date: '26-05-2024',
-    image: monacoImg,
-    link: 1068
+    date: '23-05-2024 t/m 26-05-2024',
+    image: monacoImg
   },
   {
     round: 6,
     name: 'Grand Prix van Spanje',
-    date: '21-06-2024',
-    image: spainImg,
-    link: 1069
+    date: '21-06-2024 t/m 23-06-2024',
+    image: spainImg
   },
   {
     round: 7,
     name: 'Grand Prix van Oostenrijk',
-    date: '30-06-2024',
-    image: austriaImg,
-    link: 1070
+    date: '28-06-2024 t/m 30-06-2024',
+    image: austriaImg
   },
   {
     round: 8,
     name: 'Grand Prix van Groot Brittannië',
-    date: '05-07-2024',
-    image: greatBritainImg,
-    link: 1071
+    date: '05-07-2024 t/m 07-07-2024',
+    image: greatBritainImg
   },
   {
     round: 9,
     name: 'Grand Prix van Hongarije',
-    date: '21-07-2024',
-    image: hungaryImg,
-    link: 1072
+    date: '19-07-2024 t/m 21-07-2024',
+    image: hungaryImg
   },
   {
     round: 10,
     name: 'Grand Prix van België',
-    date: '28-07-2024',
-    image: belgiumImg,
-    link: 1073
+    date: '26-07-2024 t/m 28-07-2024',
+    image: belgiumImg
   },
   {
     round: 11,
     name: 'Grand Prix van Italië',
-    date: '01-09-2024',
-    image: italyImg,
-    link: 1074
+    date: '30-08-2024 t/m 01-09-2024',
+    image: italyImg
   },
   {
     round: 12,
     name: 'Grand Prix van Azerbeidzjan',
-    date: '15-08-2024',
-    image: azerbaijanImg,
-    link: 1075
+    date: '13-08-2024 t/m 15-08-2024',
+    image: azerbaijanImg
   },
   {
     round: 13,
     name: 'Grand Prix van Qatar',
-    date: '01-12-2024',
-    image: qatarImg,
-    link: 1076
+    date: '29-11-2024 t/m 01-12-2024',
+    image: qatarImg
   },
   {
     round: 14,
     name: 'Grand Prix van Abu Dhabi',
-    date: '08-12-2024',
-    image: abuDhabiImg,
-    link: 1077
+    date: '06-12-2024 t/m 08-12-2024',
+    image: abuDhabiImg
   }
-] 
+]
