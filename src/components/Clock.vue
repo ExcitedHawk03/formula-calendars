42832f4e248d9045f4e15ef684591e3b8e879bc2
ExcitedHawk03 <joeytoet14@gmail.com>
2024-05-15 15:36:48 +0200

    Added Clock Component

---
+++ src/components/Clock.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="clock-container">
+    <p id="clock"></p>
+  </div>
+</template>
+
+<script>
+import moment from 'moment'
+
+export default {
+  name: 'Clock',
+  created() {
+    window.setInterval(() => {
+      document.getElementById('clock').innerHTML = moment().format('dddd MM/DD/y H:mm:ss')
+    }, 1000)
+  }
+}
+</script>
+
+<style scoped>
+.clock-container {
+  background-color: greenyellow;
+  color: white;
+  font-weight: bold;
+  font-size: larger;
+}
+</style>
