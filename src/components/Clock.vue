fd5ee4d97d38e693fbab35d0fa9b39da8f7cd409
ExcitedHawk03 <joeytoet14@gmail.com>
2024-05-15 16:04:50 +0200

    Added image

--- src/components/Clock.vue
+++ src/components/Clock.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="clock-container">
+    <img src="../assets/images/other/clock.png" alt="clock" />
     <p id="clock"></p>
   </div>
 </template>
 
 <script>
 import moment from 'moment'
+import 'moment/locale/nl'
 
 export default {
   name: 'Clock',
   created() {
     window.setInterval(() => {
-      document.getElementById('clock').innerHTML = moment().format('dddd MM/DD/y H:mm:ss')
+      document.getElementById('clock').innerHTML = moment().format('dddd DD/MM/y H:mm:ss')
     }, 1000)
   }
 }
@@ -19,9 +21,19 @@ export default {
 
 <style scoped>
 .clock-container {
-  background-color: greenyellow;
+  display: flex;
+  align-items: center;
+  background-color: blue;
   color: white;
   font-weight: bold;
   font-size: larger;
+  gap: 15px;
+  border-radius: 25px;
+}
+
+img {
+  width: 35px;
+  height: 35px;
+  object-fit: fill;
 }
 </style>
